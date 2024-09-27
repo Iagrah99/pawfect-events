fba65edbfcfb45247ed280ef29dd16af615f7031
Ian Graham <i4ng1999@gmail.com>
2024-09-27 17:41:56 +0100

    Move user profile jsx into UserCard component

--- src/pages/UserById.jsx
+++ src/pages/UserById.jsx
@@ -1,11 +1,10 @@
-import { useNavigate } from "react-router-dom";
-import NavigationBar from "../components/NavigationBar";
-import Breadcrumb from 'react-bootstrap/Breadcrumb';
 import { useEffect, useState } from "react";
-import { useParams, useLocation } from "react-router-dom";
+import { useParams, useLocation, useNavigate } from "react-router-dom";
 import { fetchEvents, fetchEventsAttending, fetchUserById } from "../../utils/api";
+import NavigationBar from "../components/NavigationBar";
 import Loading from "../components/Loading";
 import Error from "../components/Error";
+import UserCard from "../components/UserCard";
 
 const UserById = () => {
   const navigate = useNavigate();
@@ -74,96 +73,7 @@ const UserById = () => {
       {isLoading ? (
         <Loading content={"User Details"} />
       ) : (
-        <article className="max-w-5xl mx-auto mt-5 p-6 bg-gray-900 shadow-md rounded-lg">
-          <div className="flex justify-between items-center mb-4">
-            <Breadcrumb>
-              <Breadcrumb.Item href="/">Home</Breadcrumb.Item>
-              <Breadcrumb.Item active className="text-white">
-                {user?.username}
-              </Breadcrumb.Item>
-            </Breadcrumb>
-
-            <button
-              onClick={() => navigate(-1)}
-              className="text-white bg-cyan-600 hover:bg-cyan-700 px-4 py-2 rounded-lg shadow-md"
-            >
-              Back
-            </button>
-          </div>
-
-          {user ? (
-            <>
-              <div className="flex flex-col md:flex-row items-start">
-                <div className="w-full md:w-1/3">
-                  <img
-                    src={user.avatar_url}
-                    alt={`${user.username}'s profile`}
-                    className="w-full h-auto rounded-lg border-2 border-gray-900"
-                  />
-                </div>
-
-                <div className="w-full md:w-2/3 md:pl-6 mt-0 md:mt-0">
-                  <p className="text-2xl font-bold mb-2 text-white">
-                    {user.username} <span>({user.is_organiser ? "Event Organiser" : "Member"})</span>
-                  </p>
-
-                  {/* Show the message if it's visible */}
-                  {showDeletedMessage && (
-                    <p className="mt-3 text-green-500 font-semibold">
-                      The event has been successfully deleted!
-                    </p>
-                  )}
-
-                  <div className="mt-8 p-4 bg-gray-800 rounded-lg">
-                    {!user.is_organiser ? (
-                      <>
-                        <h2 className="text-xl font-bold mb-4 text-white">Events Attending</h2>
-                        <ul className={`list-disc text-white ${eventsAttending.length ? 'pl-5' : 'pl-0'}`}>
-                          {eventsAttending.length ? (
-                            eventsAttending.map((event) => (
-                              <li key={event} className="cursor-pointer hover:text-cyan-200"
-                                onClick={() => {
-                                  const matchedEvent = events.find(e => e.title === event)
-                                  navigate(`/events/${matchedEvent.event_id}`)
-                                }}>
-                                {event}
-                              </li>
-                            ))
-                          ) : (
-                            <p>User is not attending any events</p>
-                          )}
-                        </ul>
-                      </>
-                    ) : (
-                      <>
-                        <h2 className="text-xl font-bold mb-4 text-white">Events Organised</h2>
-                        <ul className={`list-disc text-white ${eventsAttending.length ? 'pl-5' : 'pl-0'}`}>
-                          {organiserEvents.length ? (
-                            organiserEvents.map((event) => (
-                              <li
-                                key={event.event_id}
-                                className="cursor-pointer hover:text-cyan-200"
-                                onClick={() => {
-                                  navigate(`/events/${event.event_id}`)
-                                }}
-                              >
-                                {event.title}
-                              </li>
-                            ))
-                          ) : (
-                            <p>User has not organised any events</p>
-                          )}
-                        </ul>
-                      </>
-                    )}
-                  </div>
-                </div>
-              </div>
-            </>
-          ) : (
-            <p className="text-white">No user details available</p>
-          )}
-        </article>
+        <UserCard navigate={navigate} user={user} showDeletedMessage={showDeletedMessage} organiserEvents={organiserEvents} eventsAttending={eventsAttending} />
       )}
     </>
   );
