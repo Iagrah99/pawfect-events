e087fd91f49c4ccd7d42f5d3e5e6c4bbd765434c
Ian Graham <i4ng1999@gmail.com>
2024-09-27 17:07:21 +0100

    Implement event filter, sort & order by options

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,12 +5,19 @@ import Loading from "../components/Loading";
 import { fetchEvents } from "../../utils/api"
 import EventCard from "../components/EventCard";
 import Error from "../components/Error";
+import { useSearchParams } from "react-router-dom";
 
 const Home = () => {
   const [events, setEvents] = useState([]);
+  const [filteredEvents, setFilteredEvents] = useState(null);
   const [isLoading, setIsLoading] = useState(false)
   const [isError, setIsError] = useState(false)
   const [error, setError] = useState(null)
+
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const sortByQuery = searchParams.get("sort_by");
+  const orderByQuery = searchParams.get("order_by") && searchParams.get("order_by").toUpperCase();
 
   useEffect(() => {
     setIsLoading(true)
@@ -18,17 +25,40 @@ const Home = () => {
       setIsError(false)
       setError(null)
       const fetchData = async () => {
-        const eventsFromApi = await fetchEvents();
+        const eventsFromApi = await fetchEvents(sortByQuery, orderByQuery);
         setEvents(eventsFromApi)
         setIsLoading(false)
       }
       fetchData();
     } catch (err) {
+      setIsLoading(false)
       setIsError(true)
       setError(err.response)
       console.log(err.response)
     }
-  }, [])
+  }, [sortByQuery, orderByQuery])
+
+  const handleSortBy = (e) => {
+    const newParams = new URLSearchParams(searchParams);
+    newParams.set("sort_by", e.target.value);
+    setSearchParams(newParams);
+  };
+
+  const handleOrderBy = (e) => {
+    const newParams = new URLSearchParams(searchParams);
+    newParams.set("order_by", e.target.value);
+    setSearchParams(newParams);
+  }
+
+  const handleFilterBy = (e) => {
+    if (e.target.value == "All") {
+      setFilteredEvents(events)
+      return;
+    }
+    const matchingEvents = events.filter((event) => event.event_type === e.target.value)
+    console.log(matchingEvents)
+    setFilteredEvents(matchingEvents.length > 0 ? matchingEvents : []);
+  }
 
   if (isError) {
     return (<Error error={error} />)
@@ -37,22 +67,75 @@ const Home = () => {
   return (
     <Container fluid="xs">
       <NavigationBar />
-      {isLoading ? (<Loading content="Events" />) : (
+      {isLoading ? (
+        <Loading content="Events" />
+      ) : (
         <>
           <header className="text-5xl my-5 text-center">
             <h1>Available Events</h1>
           </header>
-          <main className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 px-3 sm:px-1 pb-3">
 
-            {events.map((event) => {
-              return (
+          <div className=" p-4 rounded-lg mb-6 flex flex-col md:flex-row justify-center items-center space-y-4 md:space-y-0 md:space-x-6">
+
+            <div className="flex flex-col">
+
+              <select id="filterBy" className="mt-1 p-2 block w-full md:w-52 border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500 cursor-pointer"
+                defaultValue=""
+                onChange={(e) => handleFilterBy(e)}>
+                <option value="" disabled>
+                  Filter by Event Type
+                </option>
+                <option value="All">All Events</option>
+                <option value="Dog Training">Dog Training</option>
+                <option value="Dog Walking">Dog Walking</option>
+                <option value="Dog Show">Dog Show</option>
+                <option value="Dog Competition">Dog Competition</option>
+                <option value="Agility Trials">Agility Trials</option>
+                <option value="Herding Trials">Herding Trials</option>
+              </select>
+            </div>
+
+            <div className="flex flex-col">
+              <select id="sortBy" className="mt-1 p-2 block w-full md:w-52 border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500 cursor-pointer"
+                defaultValue=""
+                onChange={(e) => handleSortBy(e)}>
+                <option value="" disabled>
+                  Sort by
+                </option>
+                <option value="title">Title</option>
+                <option value="price_in_pence">Price</option>
+                <option value="start_date">Start Date</option>
+              </select>
+            </div>
+
+            <div className="flex flex-col">
+              <select id="orderBy" className="mt-1 p-2 block w-full md:w-52 border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500 cursor-pointer"
+                defaultValue=""
+                onChange={(e) => handleOrderBy(e)}>
+                <option value="" disabled>
+                  Order by
+                </option>
+                <option value="asc">Ascending</option>
+                <option value="desc">Descending</option>
+              </select>
+            </div>
+          </div>
+
+          <main className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 px-3 sm:px-1 pb-3">
+            {filteredEvents && filteredEvents.length === 0 ? (
+              <p className="text-center col-span-full">No events found for the selected event type.</p>
+            ) : (
+              (filteredEvents && filteredEvents.length > 0 ? filteredEvents : events).map((event) => (
                 <EventCard key={event.event_id} event={event} />
-              );
-            })}
+              ))
+            )}
           </main>
-        </>)}
+
+        </>
+      )}
     </Container>
-  )
+  );
+
 }
 
 export default Home
--- utils/api.js
+++ utils/api.js
@@ -11,7 +11,7 @@ const dogImageApi = axios.create({
 
 export const fetchEvents = async (sort_by, order_by) => {
   const res = await dogEventsApi.get(`/events`, {
-    params: { sort_by: sort_by, order_by: order_by },
+    params: { sort_by, order_by },
   });
   return res.data.events;
 };
