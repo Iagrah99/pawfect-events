b0a417902132a12521f5bbcde1635edd4040a02c
Ian Graham <i4ng1999@gmail.com>
2024-09-27 17:16:43 +0100

    Move filter, sort & order by jsx into own component

---
+++ src/components/EventControls.jsx
@@ -0,0 +1,51 @@
+const EventControls = ({ handleFilterBy, handleSortBy, handleOrderBy }) => {
+  return (
+    <div className=" p-4 rounded-lg mb-6 flex flex-col md:flex-row justify-center items-center space-y-4 md:space-y-0 md:space-x-6">
+
+      <div className="flex flex-col">
+
+        <select id="filterBy" className="mt-1 p-2 block w-full md:w-52 border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500 cursor-pointer"
+          defaultValue=""
+          onChange={(e) => handleFilterBy(e)}>
+          <option value="" disabled>
+            Filter by Event Type
+          </option>
+          <option value="All">All Events</option>
+          <option value="Dog Training">Dog Training</option>
+          <option value="Dog Walking">Dog Walking</option>
+          <option value="Dog Show">Dog Show</option>
+          <option value="Dog Competition">Dog Competition</option>
+          <option value="Agility Trials">Agility Trials</option>
+          <option value="Herding Trials">Herding Trials</option>
+        </select>
+      </div>
+
+      <div className="flex flex-col">
+        <select id="sortBy" className="mt-1 p-2 block w-full md:w-52 border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500 cursor-pointer"
+          defaultValue=""
+          onChange={(e) => handleSortBy(e)}>
+          <option value="" disabled>
+            Sort by
+          </option>
+          <option value="title">Title</option>
+          <option value="price_in_pence">Price</option>
+          <option value="start_date">Start Date</option>
+        </select>
+      </div>
+
+      <div className="flex flex-col">
+        <select id="orderBy" className="mt-1 p-2 block w-full md:w-52 border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500 cursor-pointer"
+          defaultValue=""
+          onChange={(e) => handleOrderBy(e)}>
+          <option value="" disabled>
+            Order by
+          </option>
+          <option value="asc">Ascending</option>
+          <option value="desc">Descending</option>
+        </select>
+      </div>
+    </div>
+  )
+}
+
+export default EventControls
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,6 +6,7 @@ import { fetchEvents } from "../../utils/api"
 import EventCard from "../components/EventCard";
 import Error from "../components/Error";
 import { useSearchParams } from "react-router-dom";
+import EventControls from "../components/EventControls";
 
 const Home = () => {
   const [events, setEvents] = useState([]);
@@ -38,6 +39,16 @@ const Home = () => {
     }
   }, [sortByQuery, orderByQuery])
 
+  const handleFilterBy = (e) => {
+    if (e.target.value == "All") {
+      setFilteredEvents(events)
+      return;
+    }
+    const matchingEvents = events.filter((event) => event.event_type === e.target.value)
+    console.log(matchingEvents)
+    setFilteredEvents(matchingEvents.length > 0 ? matchingEvents : []);
+  }
+
   const handleSortBy = (e) => {
     const newParams = new URLSearchParams(searchParams);
     newParams.set("sort_by", e.target.value);
@@ -48,16 +59,6 @@ const Home = () => {
     const newParams = new URLSearchParams(searchParams);
     newParams.set("order_by", e.target.value);
     setSearchParams(newParams);
-  }
-
-  const handleFilterBy = (e) => {
-    if (e.target.value == "All") {
-      setFilteredEvents(events)
-      return;
-    }
-    const matchingEvents = events.filter((event) => event.event_type === e.target.value)
-    console.log(matchingEvents)
-    setFilteredEvents(matchingEvents.length > 0 ? matchingEvents : []);
   }
 
   if (isError) {
@@ -75,51 +76,7 @@ const Home = () => {
             <h1>Available Events</h1>
           </header>
 
-          <div className=" p-4 rounded-lg mb-6 flex flex-col md:flex-row justify-center items-center space-y-4 md:space-y-0 md:space-x-6">
-
-            <div className="flex flex-col">
-
-              <select id="filterBy" className="mt-1 p-2 block w-full md:w-52 border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500 cursor-pointer"
-                defaultValue=""
-                onChange={(e) => handleFilterBy(e)}>
-                <option value="" disabled>
-                  Filter by Event Type
-                </option>
-                <option value="All">All Events</option>
-                <option value="Dog Training">Dog Training</option>
-                <option value="Dog Walking">Dog Walking</option>
-                <option value="Dog Show">Dog Show</option>
-                <option value="Dog Competition">Dog Competition</option>
-                <option value="Agility Trials">Agility Trials</option>
-                <option value="Herding Trials">Herding Trials</option>
-              </select>
-            </div>
-
-            <div className="flex flex-col">
-              <select id="sortBy" className="mt-1 p-2 block w-full md:w-52 border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500 cursor-pointer"
-                defaultValue=""
-                onChange={(e) => handleSortBy(e)}>
-                <option value="" disabled>
-                  Sort by
-                </option>
-                <option value="title">Title</option>
-                <option value="price_in_pence">Price</option>
-                <option value="start_date">Start Date</option>
-              </select>
-            </div>
-
-            <div className="flex flex-col">
-              <select id="orderBy" className="mt-1 p-2 block w-full md:w-52 border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500 cursor-pointer"
-                defaultValue=""
-                onChange={(e) => handleOrderBy(e)}>
-                <option value="" disabled>
-                  Order by
-                </option>
-                <option value="asc">Ascending</option>
-                <option value="desc">Descending</option>
-              </select>
-            </div>
-          </div>
+          <EventControls handleFilterBy={handleFilterBy} handleSortBy={handleSortBy} handleOrderBy={handleOrderBy} />
 
           <main className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 px-3 sm:px-1 pb-3">
             {filteredEvents && filteredEvents.length === 0 ? (
